9c466d75b0a862e7aaddd9160fafb9f3d1dc837a
Marco Marques <marcohenriquejava@gmail.com>
2016-09-22 00:54:44 -0300

    Primeira parte da tela de visitante.

--- www/js/app.js
+++ www/js/app.js
@@ -4,191 +4,232 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic','starter.controllers', 'login.controller', 'auth.services', 'interceptor.factory', 'condominio.controller', 'condominio.service'])
+angular.module('starter', ['ionic', 'starter.controllers', 'login.controller', 'auth.services', 'interceptor.factory', 'condominio.controller', 'condominio.service',
+                                                                      'visitante.controller', 'visitante.service'])
 //angular.module('starter', ['ionic'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+  .run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.run(function ($rootScope, $state, AuthService, AUTH_EVENTS) {
-  $rootScope.$on('$stateChangeStart', function (event,next, nextParams, fromState) {
- 
-    if ('data' in next && 'authorizedRoles' in next.data) {
-      var authorizedRoles = next.data.authorizedRoles;
-      if (!AuthService.isAuthorized(authorizedRoles)) {
-        event.preventDefault();
-        $state.go($state.current, {}, {reload: true});
-        $rootScope.$broadcast(AUTH_EVENTS.notAuthorized);
       }
-    }
- 
-    if (!AuthService.isAuthenticated()) {
-      if (next.name !== 'login') {
-        event.preventDefault();
-        $state.go('login');
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleDefault();
       }
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider, USER_ROLES) {
-  $stateProvider
-
-  .state('login', {
-    url: '/login',
-    templateUrl: 'templates/login.html',
-    controller: 'LoginCtrl'
+    });
   })
 
-  .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
-  })
+  .run(function ($rootScope, $state, AuthService, AUTH_EVENTS) {
+    $rootScope.$on('$stateChangeStart', function (event, next, nextParams, fromState) {
 
-  .state('app.search', {
-    url: '/search',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/search.html'
-      }
-    },
-    data: {
-      authorizedRoles: [USER_ROLES.admin]
-    }
-
-  })
-
-  .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
+      if ('data' in next && 'authorizedRoles' in next.data) {
+        var authorizedRoles = next.data.authorizedRoles;
+        if (!AuthService.isAuthorized(authorizedRoles)) {
+          event.preventDefault();
+          $state.go($state.current, {}, {reload: true});
+          $rootScope.$broadcast(AUTH_EVENTS.notAuthorized);
         }
       }
-    })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
+
+      if (!AuthService.isAuthenticated()) {
+        if (next.name !== 'login') {
+          event.preventDefault();
+          $state.go('login');
         }
       }
-    })
-    .state('app.condominios', {
-      url: '/condominios',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/condominios.html',
-          resolve: {
-                condominios: function(CondominioService){
-                    return CondominioService.getLista();
-                },
-                condominio: function(){
-                    return {};
-                }
-          },
+    });
+  })
 
-          controller: 'CondominioCtrl'
+  .config(function ($stateProvider, $urlRouterProvider, USER_ROLES) {
+    $stateProvider
+
+      .state('login', {
+        url: '/login',
+        templateUrl: 'templates/login.html',
+        controller: 'LoginCtrl'
+      })
+
+      .state('app', {
+        url: '/app',
+        abstract: true,
+        templateUrl: 'templates/menu.html',
+        controller: 'AppCtrl'
+      })
+
+      .state('app.search', {
+        url: '/search',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/search.html'
+          }
+        },
+        data: {
+          authorizedRoles: [USER_ROLES.admin]
         }
-      },
-      data: {
-        authorizedRoles: [USER_ROLES.admin]
-      }
-    })
 
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
-      }
-    }
-  })
+      })
 
-  .state('app.condominio', {
-    url: '/condominios/:condominioId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/condominio.html',
-        resolve: {
-                condominios: function(){
-                    return {};
-                },  
-                condominio: function(CondominioService, $stateParams){
-                    return CondominioService.get($stateParams.condominioId);
-                }
+      .state('app.browse', {
+        url: '/browse',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/browse.html'
+          }
+        }
+      })
+      .state('app.playlists', {
+        url: '/playlists',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/playlists.html',
+            controller: 'PlaylistsCtrl'
+          }
+        }
+      })
+      .state('app.condominios', {
+        url: '/condominios',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/condominios.html',
+            resolve: {
+              condominios: function (CondominioService) {
+                return CondominioService.getLista();
+              },
+              condominio: function () {
+                return {};
+              }
+            },
+
+            controller: 'CondominioCtrl'
+          }
         },
-        controller: 'CondominioCtrl'
-      }
-    },
-    data: {
-        authorizedRoles: [USER_ROLES.admin]
-      }
-  })
+        data: {
+          authorizedRoles: [USER_ROLES.admin]
+        }
+      })
+      .state('app.visitantes', {
+        url: '/visitantes',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/visitantes.html',
+            resolve: {
+              visitantes: function (VisitanteService) {
+                return VisitanteService.getListaDeVisitantes();
+              },
+              visitante: function () {
+                return {};
+              }
+            },
 
-  .state('app.condominioEditar', {
-    url: '/condominios/editar/:condominioId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/condominio_editar.html',
-        resolve: {
-                condominios: function(){
-                    return {};
-                },
-                condominio: function(CondominioService, $stateParams){
-                    return CondominioService.get($stateParams.condominioId);
-                }
+            controller: 'VisitanteCtrl'
+          }
         },
-        controller: 'CondominioCtrl'
-      }
-    },
-    data: {
-        authorizedRoles: [USER_ROLES.admin]
-      }
-  })
+        data: {
+          authorizedRoles: [USER_ROLES.admin]
+        }
+      })
+      .state('app.visitanteEditar', {
+        url: '/visitantes/editar/:visitanteId',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/visitante_editar.html',
+            resolve: {
+              visitantes: function () {
+                return {};
+              },
+              visitante: function (VisitanteService, $stateParams) {
+                return VisitanteService.get($stateParams.visitanteId);
+              }
+            },
+            controller: 'VisitanteCtrl'
+          }
+        },
+        data: {
+          authorizedRoles: [USER_ROLES.admin]
+        }
+      })
+      .state('app.single', {
+        url: '/playlists/:playlistId',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/playlist.html',
+            controller: 'PlaylistCtrl'
+          }
+        }
+      })
 
-  .state('app.condominioAdicionar', {
-    url: '/condominios/adicionar/',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/condominio_adicionar.html',
-        resolve: {
-                condominios: function(){
-                    return {};
-                },
-                condominio: function(CondominioService, $stateParams){
-                    return {};
-                }
+      .state('app.condominio', {
+        url: '/condominios/:condominioId',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/condominio.html',
+            resolve: {
+              condominios: function () {
+                return {};
+              },
+              condominio: function (CondominioService, $stateParams) {
+                return CondominioService.get($stateParams.condominioId);
+              }
+            },
+            controller: 'CondominioCtrl'
+          }
         },
-        controller: 'CondominioCtrl'
-      }
-    },
-    data: {
-        authorizedRoles: [USER_ROLES.admin]
-      }
+        data: {
+          authorizedRoles: [USER_ROLES.admin]
+        }
+      })
+
+      .state('app.condominioEditar', {
+        url: '/condominios/editar/:condominioId',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/condominio_editar.html',
+            resolve: {
+              condominios: function () {
+                return {};
+              },
+              condominio: function (CondominioService, $stateParams) {
+                return CondominioService.get($stateParams.condominioId);
+              }
+            },
+            controller: 'CondominioCtrl'
+          }
+        },
+        data: {
+          authorizedRoles: [USER_ROLES.admin]
+        }
+      })
+
+      .state('app.condominioAdicionar', {
+        url: '/condominios/adicionar/',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/condominio_adicionar.html',
+            resolve: {
+              condominios: function () {
+                return {};
+              },
+              condominio: function (CondominioService, $stateParams) {
+                return {};
+              }
+            },
+            controller: 'CondominioCtrl'
+          }
+        },
+        data: {
+          authorizedRoles: [USER_ROLES.admin]
+        }
+      });
+
+
+    $urlRouterProvider.otherwise(function ($injector, $location) {
+      var $state = $injector.get("$state");
+      $state.go("app.playlists");
+    });
+
   });
-
-   
-  $urlRouterProvider.otherwise(function ($injector, $location) {
-    var $state = $injector.get("$state");
-    $state.go("app.playlists");
-  });
-
-});
---
+++ www/js/controllers/visitanteController.js
@@ -0,0 +1,48 @@
+/**
+ * Created by marcomarques on 21/09/16.
+ */
+angular.module('visitante.controller', [])
+
+.controller('VisitanteCtrl', function($scope, $rootScope, $state, $ionicPopup, AuthService, $stateParams, visitante, visitantes,VisitanteService) {
+
+  $scope.visitantes = visitantes;
+  $scope.visitante = visitante;
+
+  $rootScope.$on('listaVisitanteAlterada', function() {
+    VisitanteService.getListaDeVisitantes().then(function (data) {
+      $scope.visitantes = data;
+    });
+  });
+
+
+  $scope.editarVisitante = function(id) {
+    $state.go('app.visitanteEditar', {visitanteId: id});
+  };
+
+
+  $scope.excluirVisitante = function(id) {
+    VisitanteService.deletar(id).then(function(data) {
+      var alertPopup = $ionicPopup.alert({
+        title: 'CondoApp',
+        template: 'Visitante removido com sucesso!'
+      });
+      $scope.$emit('listaVisitanteAlterada');
+      $state.go($state.current, {}, {
+        reload: true});
+    });
+  };
+
+
+  $scope.enviarEdicaoDeVisitante = function () {
+    VisitanteService.put($scope.visitante).then(function (visitante) {
+      var alertPopup = $ionicPopup.alert({
+        title: 'CondoApp',
+        template: 'Visitante Alterado com sucesso!'
+      });
+      $scope.$emit('listaVisitanteAlterada');
+      $state.go('app.visitantes', {visitanteId: visitante.id});
+    });
+  };
+
+
+});
